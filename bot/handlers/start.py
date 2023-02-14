91c2965b8d303b35c54ef13340c1ba0c54bbeae4
FlashLight552 <saw1997hunterone@gmail.com>
2023-02-14 17:57:44 +0200

    upd

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,15 +1,12 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import CommandStart
-from aiogram.utils.markdown import hlink
-
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-
-
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types.web_app_info import WebAppInfo
 
 from functions.sql import Database
+import os
 
 class Form(StatesGroup):
     user_name = State()
@@ -30,7 +27,7 @@ async def start(message: types.Message):
 
 async def save_name(message: types.Message, state: FSMContext):
     location_btn = ReplyKeyboardMarkup().add(KeyboardButton(text='Определить геолокацию',
-                                                            web_app=WebAppInfo(url="https://127.0.0.1:3000/")))
+                                                            web_app=WebAppInfo(url=os.environ['webapp_path'])))
     await state.finish() 
     
     text = f'Your name is {message.text}'
